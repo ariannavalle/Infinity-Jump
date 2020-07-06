e4b7cc1669198dd4b231512355d161ff7ba86822
Arianna Valle <ariannamarbella@gmail.com>
2020-07-06 12:24:45 -0400

    Remove jumping functionality and instead add gravity to player

--- js/player.js
+++ js/player.js
@@ -1,37 +1,16 @@
 class Player extends Component {
-	constructor(game, x, y, width, height, velocity) {
+	constructor(game, x, y, width, height, xVelocity, yVelocity) {
 		super(game, x, y, width, height);
-		this.velocity = velocity;
-	}
-
-	jump() {
-		/* 
-		 * if (the player's position + height is greater than the bottom of the canvas) → reverse direction
-		 * else → move up 0.2 every frame
-		 * then → move down by 0.2 
-		 */
-		if (this.y + this.height > canvas.height) {
-			this.velocity = -this.velocity;
-		} else {
-			this.velocity += 0.2;
-		}
-		this.y += this.velocity;
+		this.xVelocity = xVelocity;
+		this.yVelocity = yVelocity;
 	}
 
 	move() {
-		document.addEventListener("keydown", (event) => {
-			switch (event.keyCode) {
-				case 37:
-				case 65:
-					if (this.x >= 0) this.x-=50;
-					break;
-				case 39:
-				case 83:
-					if (this.x <= canvas.width - this.width) this.x+=50;
-					break;
-				default:
-					console.log("Invalid Key");
-			}
-		});
+		this.x += this.xVelocity;
+	}
+
+	fall() {
+		this.y =+ this.yVelocity;
+		this.yVelocity += 0.1
 	}
 }
