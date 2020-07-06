c57342297dd569d63fb6a04aedf8e3fe0ce3723f
Arianna Valle <ariannamarbella@gmail.com>
2020-07-06 12:34:35 -0400

    Add key handlers for side to side movement

--- js/game.js
+++ js/game.js
@@ -2,26 +2,59 @@ class Game {
 	constructor() {
 		this.canvas = document.querySelector("#canvas");
 		this.ctx = this.canvas.getContext("2d");
-		this.player = new Player(this, this.canvas.width/2-50, this.canvas.height-300, 100, 100, 0.1);
+		this.player = new Player(this, this.canvas.width/2-50, this.canvas.height-300, 100, 100, 0, 3);
 	}
 
 	init = () => {
-		this.animationLoop();
-		this.player.move();
+		this.handleKeys()
+		this.update();
 	};
 
-	animationLoop = () => {
+	update = () => {
 		this.clear();
 		this.drawPlayer();
-		this.player.jump();
-		requestAnimationFrame(this.animationLoop);
+		this.player.move();
+		this.player.fall();
+		requestAnimationFrame(this.update);
 	};
 	
 	clear = () => {
 		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 	};
 
-	drawPlayer() {
+	drawPlayer = () => {
 		this.player.drawComponent('./images/space-right.png')
 	}
+
+	handleKeys = () => {
+        document.addEventListener("keydown", (event) => {
+            switch (event.keyCode) {
+                case 37: //left arrow
+                case 65: //'a'
+                    this.player.xVelocity = -5
+                    break;
+                case 39: //right arrow
+                case 68: //'d'
+                    this.player.xVelocity = 5;
+                    break;
+                default:
+                    console.log("Invalid Key");
+            }
+        });
+
+        document.addEventListener("keyup", (event) => {
+            switch (event.keyCode) {
+                case 37:
+                case 65:
+                    this.player.xVelocity = 0;
+                    break;
+                case 39:
+                case 68:
+                    this.player.xVelocity = 0;
+                    break;
+                default:
+                    console.log("Invalid Key");
+            }
+        });
+    }
 }
