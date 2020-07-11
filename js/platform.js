aef808c50c4120138af57818e33baa970d1959f4
Arianna Valle <ariannamarbella@gmail.com>
2020-07-11 14:26:19 -0400

    Begin implementing exploding platforms

--- js/platform.js
+++ js/platform.js
@@ -2,13 +2,14 @@ class Platform extends Component {
 	constructor(game, x, y, width, height) {
 		super(game, x, y, width, height);
 		this.playerOnTop = false; //the bottom of player needs to be on top of the platform to bounce off of it
+		this.hasSpring = false;
+		this.explodes = false;
+		this.explosionPossibility = Math.floor(Math.random() * 5);
+		this.moves = false;
 	}
 
 	increaseY = () => {
 		this.y -= this.game.player.yVelocity * 0.01; 
-		this.game.player.yVelocity += 0.1; 
-
-		this.game.score += 0.001
 	}
 
 	checkCollision = () => {
@@ -16,6 +17,7 @@ class Platform extends Component {
             this.playerOnTop = true;
 		}
 		
+		//normal - player jumps when collides with platform 
 		if (
 			this.game.player.x+30 < this.x + this.width &&
 			this.game.player.x-30 + this.game.player.width > this.x &&
@@ -25,6 +27,19 @@ class Platform extends Component {
 		) {
 			this.game.player.yVelocity = -1200; //determines how high the player will bounce up 
 			this.game.player.makeSound('../sfx/jump.wav');
+		}
+
+		//platform explodes 
+		if (this.explodes === true) {
+			if (
+				this.game.player.x+30 < this.x + this.width &&
+				this.game.player.x-30 + this.game.player.width > this.x &&
+				this.game.player.y < this.y + this.height &&
+				this.game.player.y + this.game.player.height > this.y &&
+				this.playerOnTop && this.explodes
+			) {
+				this.game.player.makeSound('../sfx/explodingplatform.mp3');
+			}
 		}
 	};
 }
